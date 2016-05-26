36e0b157f7a0b6b091f8b3e5c54144a992292f6d
wibo-hyve <wibo@thehyve.nl>
2016-05-26 17:04:06 +0200

    Added heatmap part of the tutorial

--- sorlie.R
+++ sorlie.R
@@ -22,6 +22,12 @@
 
 # For transmartRClient installation see https://github.com/thehyve/RInterface
 
+#############################################################################
+#                             Introduction                                  #
+#############################################################################
+
+
+
 # Example steps to authenticate with, connect to, and retrieve data from tranSMART
 # load package
 require("transmartRClient")
@@ -36,7 +42,9 @@ print(studies)
 
 # to access the studies programmatically use for example: 
 # study<-studies$id[1]
-# for the examples below we will use GSE4382, a study that ???
+# for the examples below we will use GSE4382, which is called "Sorlie(2003) GSE4382" in transmart.
+# This also shows that a study name is not the same as a study ID, all the data retrieval is done
+# study ID rather than the name. You can use View() to have a closer look at the studies in transmart.
 study <- "GSE4382_ETRIKS"  
 
 # Retrieve Clinical Data
@@ -49,19 +57,19 @@ allObservations <- getObservations(study, as.data.frame = T)
 #    found in the data tree  of tranSMART (see the panel "Navigate terms" in the "Analyze" tab of the 
 #    tranSMART web app: http://transmart-demo.thehyve.net/transmart/datasetExplorer/index ) 
 summary(allObservations)
-print(allObservations$observations[1:12,1:7]) #hint: if you are using RStudio you can also use the function "View" to see the data in a more user-friendly table, e.g.: View(allObservations$observations)
-print(allObservations$subjectInfo[1:12,])
-print(allObservations$conceptInfo[1:10,])
+allObservations$observations[1:12,1:7] #hint: if you are using RStudio you can also use the function "View" to see the data in a more user-friendly table, e.g.: View(allObservations$observations)
+allObservations$subjectInfo[1:12,]
+allObservations$conceptInfo[1:10,]
 
 # retrieve information about the concepts that are present for this study
 concepts <- getConcepts(study)
-print(concepts)  
+concepts
 
 # You can use the concept names or the concept links to only retrieve data for a subset of the variables:
 
-# retrieve observations for the first concept which conceptname contains "Tumor Subtypes"
+# retrieve observations for the first concept which concept name contains "Tumor Subtypes"
 observations <- getObservations(study, concept.match = "Tumor Subtypes", as.data.frame = T)
-print(observations$observations)
+observations$observations
 
 # retrieve observations belonging to the first two concepts by using the api.links contained in the getConcepts-result
 observations <- getObservations(study, concept.links = concepts$api.link.self.href[c(1,2)])
@@ -88,32 +96,61 @@ dataDownloaded$data[1:10,1:10]
 expression_data<-dataDownloaded$data[,-c(1:6)]
 dim(expression_data)
 rownames(expression_data)<-dataDownloaded$data$patientId
+expression_data[1:10,1:5]
 
 #Make a heatmap
 #If the dimensions of the expression_data table are large, you may want to create a subset of the data first. 
 
 # select only the cases and controls (excluding the patients for which the lung disease is not specified). Note: in the observation table the database IDs 
 # are used to identify the patients and not the patient IDs that are used in the gene expression dataset
-# TODO Define case and control properly
-cases <- allObservations$observations$subject.id[allObservations$observations$'Subjects_Medical History_Tumor Characteristics_Tumor Subtypes' %in% c('Basal', 'ERBB2', 'Luminal A', 'Luminal B')]
-controls <- setdiff(allObservations$observations$subject.id, cases)
 
-# now we have the database IDs for the patients, but we need to get the patient IDs. These can be retrieved from the subjectInfo table: 
-subjectInfo <- allObservations$subjectInfo
-patientIDsCase    <- subjectInfo$subject.inTrialId[ subjectInfo$subject.id %in% cases ] 
-patientIDsControl <- subjectInfo$subject.inTrialId[ subjectInfo$subject.id %in% controls] 
+library(Cairo)
+library(ggplot2)
+library(reshape2)
+library(gplots)
+library(plyr)
 
-patientSet <- c(patientIDsCase,patientIDsControl)
+# Create a dataframe where it is clear which patients are part of what group, remove the patients that do not have a subgroup
+tmpData = merge(allObservations$subjectInfo[c("subject.id","subject.inTrialId")],allObservations$observations,by="subject.id")
+groups <- tmpData[c("subject.inTrialId","Subjects_Medical History_Tumor Characteristics_Tumor Subtypes")]
+colnames(groups)<- c("subjects","tumorSubtypes")
+groups <- groups[groups$"tumorSubtypes"!="None",]
+#View(groups) # you can use this to have a look in Rstudio, uncomment first
+expression_data <- expression_data[row.names(expression_data)%in% groups$subjects,]
+#View(expression_data) # Correct subject IDs are included. Can view in Rstudio if you like.
 
-patientSet<-patientSet[which(patientSet %in% rownames(expression_data))]
-#make a subset of the data based on the selected patientSet and the probelist, and transpose the table so that the rows now contain probe names
-#??? TODO Select probse
-probeNames <- colnames(expression_data)[1:100]
-subset<-t(expression_data[patientSet,probeNames]) 
+## The following genes are a selection from all the genes used in the paper. Per subgroup a few genes have been used.
+ERBB2 <- c("TLK2","MED24","MED1","ERBB2","GRB7") 
+LumB <- c("ATP5G1","ERI3","CSDA","GGH","LAPTM4B","PRDX4","CCNE1")
+LumA <- c("NAT1","SLC39A6","FOXA3","XBP1","GATA3","ESR1","PTP4A2","RERG","SCUBE2")
+basal <- c("CXCL1","CDH3","ANXA8L1","KRT5","TRIM29","KRT17","CX3CL1","FZD8","CHI3L2","B3GNT5")
+normal_breast <- c("PIK3R1","AKR1C1","ACSL1")
+# gene list
+genes <- c(ERBB2,LumA,LumB,basal,normal_breast)
 
-#for ease of recognition: append "Case" and "Control" to the patient names
-colnames(subset)[colnames(subset)%in% patientIDsCase] <- paste(colnames(subset)[colnames(subset)%in% patientIDsCase],"Case", sep="_" )
-colnames(subset)[colnames(subset)%in% patientIDsControl] <- paste( colnames(subset)[colnames(subset)%in% patientIDsControl] , "Control",sep= "_")
+# Transpose the expression_data to make it ready for generating a heatmap
+mRNAdata <- t(expression_data[genes])
 
-dim(subset)
-heatmap(as.matrix(subset), scale = "row")
+## expression data group labeling.
+for (i in unique(groups$tumorSubtypes)){
+  print(i)
+  colnames(mRNAdata)[colnames(mRNAdata) %in% groups[groups$tumorSubtypes==i,"subjects"]] <- paste(
+    i,colnames(mRNAdata)[colnames(mRNAdata) %in% groups[groups$tumorSubtypes==i,"subjects"]],sep="_")
+}
+
+## Generate a vector for the column corresponding to the groups.
+colcolor<- colnames(mRNAdata)
+colcolor[grep("Basal",colnames(mRNAdata))]<-"firebrick4"
+colcolor[grep("Luminal A",colnames(mRNAdata))]<-"dodgerblue4"
+colcolor[grep("Luminal B",colnames(mRNAdata))]<-"dodgerblue1"
+colcolor[grep("ERBB2",colnames(mRNAdata))]<-"deeppink3"
+colcolor[grep("Normal",colnames(mRNAdata))]<-"chartreuse4"
+
+## generate heatmap ##
+heatmap.2(mRNAdata,
+          Rowv = TRUE,
+          Colv = TRUE,
+          trace="none",
+          col = greenred(800),
+          ColSideColors = colcolor
+)
